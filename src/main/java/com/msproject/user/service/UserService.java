7a0d56dbddb4dedd1eeba653ce78b9fe7418ddac
Andrew Williams <1williamsaj@gmail.com>
2021-10-15 10:33:11 -0500

    Removed fallback method from user controller and service

--- src/main/java/com/msproject/user/service/UserService.java
+++ src/main/java/com/msproject/user/service/UserService.java
@@ -72,19 +72,5 @@ public class UserService {
 		return vo;
 	}
 
-	public User userCreateFail(Exception e) {
-		// TODO Auto-generated method stub
-		log.info("Inside userCreateFail inside of UserService");
-		
-		User user = new User();
-		
-		user.setUserId(0L);
-		user.setFirstName("First - FAILED");
-		user.setLastName("Last - FAILED");
-		user.setEmail("Unknown - FAILED");
-		user.setDepartmentId(0L);
-		
-		return user;
-	}
 	
 }
